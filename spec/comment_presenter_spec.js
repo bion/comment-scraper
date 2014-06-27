b9641f9545ef2833dc211d26dde57a1b657a2a03
bion <bionjohnson@gmail.com>
2014-06-26 23:15:07 -0700

    Add CommentPresenter#commentBody

--- spec/comment_presenter_spec.js
+++ spec/comment_presenter_spec.js
@@ -8,7 +8,7 @@ describe('CommentPresenter', function() {
         beforeEach(function() {
             this.commentWithImage = "<div>  <div>        <div><h3>B Davis (#5372)</h3></div>  </div>  <span>    <strong>Date Submitted: </strong>    <span><span class='date-display-single' property='dc:date' datatype='xsd:dateTime' content='2012-12-04T00:00:00-05:00'>12/04/12</span></span>  </span>  <div>        <div><strong>Location:</strong> Spokane, WA</div>  </div>  <div>    <strong>Comment: </strong>    <div>See attached.</div>  </div>  <div class='fr-image'>    <strong>Attached Image: </strong>    <div><a href='attachments/images/Spokane_Mtg_108.jpg'><img typeof='foaf:Image' src='attachments/images/Spokane_Mtg_108.jpg' width='630' alt='></a></div>  </div>  </div>";
 
-            this.subject = new CommentPresenter(this.commentWithImage);
+            this.subject = new CommentPresenter(this.commentWithImage, 'foo/');
         });
 
         describe('#name', function() {
@@ -41,6 +41,20 @@ describe('CommentPresenter', function() {
                 this.subject.date().should.equal('12/04/12');
             });
 
+        });
+
+        describe('#isImage', function() {
+
+            it('returns true', function() {
+                this.subject.isImage().should.equal(true);
+            });
+
+        });
+
+        describe('#commentBody', function() {
+            it('returns a link to the comment image', function() {
+                this.subject.commentBody().should.equal('foo/attachments/images/Spokane_Mtg_108.jpg');
+            });
         });
 
     });
