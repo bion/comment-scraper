2094a253a0f7bf2dbf77a6e6efdc6c88e7d55f1b
bion <bionjohnson@gmail.com>
2014-06-26 23:33:19 -0700

    \#commentBody returns text of comment when no img

--- spec/comment_presenter_spec.js
+++ spec/comment_presenter_spec.js
@@ -59,7 +59,7 @@ describe('CommentPresenter', function() {
 
     });
 
-    context('content has no location', function() {
+    context('comment has no location', function() {
 
         beforeEach(function() {
             this.commentWithoutLocation = '<div>  <div>        <div><h3>B Halliday (#1265)</h3></div>  </div>    <span>    <strong>Date Submitted: </strong>    <span><span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content="2012-10-13T00:00:00-04:00">10/13/12</span></span>  </span>    <div>        <div></div>  </div>    <div>    <strong>Comment: </strong>    <div>see attached</div>  </div>    <div class="fr-image">    <strong>Attached Image: </strong>    <div><a href="attachments/images/MailerForm158.jpg"><img typeof="foaf:Image" src="attachments/images/MailerForm158.jpg" width="630" alt=""></a></div>  </div>  </div>';
@@ -71,6 +71,24 @@ describe('CommentPresenter', function() {
 
             it("should return 'not provided'", function() {
                 this.subject.location().should.equal('not provided');
+            });
+
+        });
+
+    });
+
+    context('comment body is text', function() {
+
+        beforeEach(function() {
+            this.textComment = "<div>  <div>        <div><h3>B. Prem (#8319)</h3></div>  </div>    <span>    <strong>Date Submitted: </strong>    <span>01/17/2013</span>  </span>    <div>        <div></div>  </div>    <div>    <strong>Comment: </strong>    <div>Dear GPT/BNSF Custer Spur EIS Co-Lead Agencies,<br> <br>I am a property owner in San Juan County.  I am concerned about the continued vitality of the Salish Sea, where coal ships would make over 950 transits per year if the Gateway Pacific Terminal were to be built.  I request that the GPT Environmental Impact Statement include the entire coal transportation corridor so that communities along the rail and marine routes are given due consideration.<br> <br>I am especially concerned about oil and coal spill risks.  Questions that concern me, and which objective, rigorous and comprehensive studies should address include:<br>How will GPT's marine vessel traffic increase collision risks with tankers and other cargo ships in the area?<br>What would be the effects on our region of a catastrophic oil and/or coal spill?<br>If there is no positive assurance and insurance from those involved against any potentially significant impacts, please consider a no build option.<br> <br>Sincerely,<br><br>Prem</div>  </div>  </div>";
+
+            this.subject = new CommentPresenter(this.textComment, 'bar');
+        });
+
+        describe('#commentBody', function() {
+
+            it('should return the text of the comment', function() {
+                this.subject.commentBody().should.equal("Dear GPT/BNSF Custer Spur EIS Co-Lead Agencies, I am a property owner in San Juan County.  I am concerned about the continued vitality of the Salish Sea, where coal ships would make over 950 transits per year if the Gateway Pacific Terminal were to be built.  I request that the GPT Environmental Impact Statement include the entire coal transportation corridor so that communities along the rail and marine routes are given due consideration. I am especially concerned about oil and coal spill risks.  Questions that concern me, and which objective, rigorous and comprehensive studies should address include:How will GPT's marine vessel traffic increase collision risks with tankers and other cargo ships in the area?What would be the effects on our region of a catastrophic oil and/or coal spill?If there is no positive assurance and insurance from those involved against any potentially significant impacts, please consider a no build option. Sincerely,Prem");
             });
 
         });
