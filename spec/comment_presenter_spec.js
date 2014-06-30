a4c0576f2c40ed1e9634c94c531a8e852add9c7d
bion <bionjohnson@gmail.com>
2014-06-29 21:40:37 -0700

    Trial/error formatting changes to presenter

--- spec/comment_presenter_spec.js
+++ spec/comment_presenter_spec.js
@@ -8,13 +8,15 @@ describe('CommentPresenter', function() {
         beforeEach(function() {
             this.commentWithImage = "<div>  <div>        <div><h3>B Davis (#5372)</h3></div>  </div>  <span>    <strong>Date Submitted: </strong>    <span><span class='date-display-single' property='dc:date' datatype='xsd:dateTime' content='2012-12-04T00:00:00-05:00'>12/04/12</span></span>  </span>  <div>        <div><strong>Location:</strong> Spokane, WA</div>  </div>  <div>    <strong>Comment: </strong>    <div>See attached.</div>  </div>  <div class='fr-image'>    <strong>Attached Image: </strong>    <div><a href='attachments/images/Spokane_Mtg_108.jpg'><img typeof='foaf:Image' src='attachments/images/Spokane_Mtg_108.jpg' width='630' alt='></a></div>  </div>  </div>";
 
-            this.subject = new CommentPresenter(this.commentWithImage, 'foo/');
+            this.baseUrl = 'foo';
+
+            this.subject = new CommentPresenter(this.commentWithImage, this.baseUrl);
         });
 
         describe('#newPresenter', function() {
 
             it('returns a new presenter', function() {
-                var presenter = CommentPresenter.newPresenter(this.commentWithImage);
+                var presenter = CommentPresenter.newPresenter(this.commentWithImage, this.baseUrl);
                 Object.keys(presenter).should.eql(['comment', 'baseUrl']);
             });
         });
